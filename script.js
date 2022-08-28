20c51162675655a5b931d4bd646ac644b7e9c7c2
Ivan Rodriguez <sickpunkrocker@gmail.com>
2022-08-28 08:12:35 -0700

    Added README and Starter code provided

---
+++ script.js
@@ -0,0 +1,36 @@
+// Assignment code here
+
+
+
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About

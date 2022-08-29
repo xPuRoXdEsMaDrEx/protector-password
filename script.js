63c549e323ba251e1afaa0ac60ca509161d390d2
Ivan Rodriguez <sickpunkrocker@gmail.com>
2022-08-28 22:14:22 -0700

    Added variables to javascript file that I have decided to use and added correct html file and linked it to the CSS

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Assignment code here
 
+var lowercase = [ "a","b","c","d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "?", "<", ">", "=", "+"]
 
-
-
+lowercaseAsk = prompt("Select OK if you would like lowercase characters in your password");
+    if
 
 
 // Get references to the #generate element

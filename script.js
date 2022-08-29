c485c0c19eb26356ae9d8e6de07a14a3b869a411
Ivan Rodriguez <sickpunkrocker@gmail.com>
2022-08-29 08:36:50 -0700

    Adding what I have at the moment for Javascript file

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // Assignment code here
 
-var lowercase = [ "a","b","c","d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "?", "<", ">", "=", "+"]
+// console.log("I am linked")
 
-lowercaseAsk = prompt("Select OK if you would like lowercase characters in your password");
-    if
+// window.prompt("Enter a word")
+// window.alert()
+// window.confirm ()
+
+var lowercase = [ "a","b","c","d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "?", "<", ">", "=", "+", ".", ",", "/", "|"];
+
+
+howManyCharacters = prompt("How many characters would you like in you password? Choose between 8 and 128 characters."); 
+if (howManyCharacters < 8 || howManyCharacters > 128) {
+    prompt("You have NOT selected the appropriate amount of characters to be in your password, please choose between 8 and 128 characters.")
+} else if (howManyCharacters); {
+    alert("You have selected " + howManyCharacters + "characters to be in your password.")
+} 
+    
+console.log(howManyCharacters)
 
 
 // Get references to the #generate element
@@ -23,17 +36,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
+
